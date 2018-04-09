774cbed8892199449fde930f9ac27051010629f3
Azen Xu <xuchang08@meituan.com>
2018-04-09 13:17:45 +0800

    模拟外部数据

--- todo-app/src/TODOApp.js
+++ todo-app/src/TODOApp.js
@@ -3,8 +3,28 @@ import TODOHeader from './TODOHeader';
 import TODOInput from './TODOInput';
 import TODOList from './TODOList';
 
+//  模拟网络获取到的外部数据
 let todos = [
-    
+    {
+        id: 100,
+        title: "末世歌者",
+        checked: false
+    },
+    {
+        id: 101,
+        title: "小棋童",
+        checked: false
+    },
+    {
+        id: 102,
+        title: "马步谣",
+        checked: false
+    },
+    {
+        id: 102,
+        title: "藏",
+        checked: false
+    }
 ]
 
 class TODOApp extends Component {
