5e47681bdf69e415b3033c29a12c168850c7c042
Azen Xu <xuchang08@meituan.com>
2018-04-09 13:22:04 +0800

    根据外部数据，计算代办数量

--- todo-app/src/TODOApp.js
+++ todo-app/src/TODOApp.js
@@ -4,11 +4,12 @@ import TODOInput from './TODOInput';
 import TODOList from './TODOList';
 
 //  模拟网络获取到的外部数据
+//  需要todoCount根据checked的状态动态显示
 let todos = [
     {
         id: 100,
         title: "末世歌者",
-        checked: false
+        checked: true
     },
     {
         id: 101,
@@ -29,9 +30,15 @@ let todos = [
 
 class TODOApp extends Component {
     render() {
+
+        //  过滤外部数据，计算待办事项数
+        let todoCount = todos.filter((object) => {
+            return !object.checked;
+        }).length;
+
         return (
             <div>
-                <TODOHeader name="双笙子" todoCount={ 88 }/>
+                <TODOHeader name="双笙子" todoCount={ todoCount }/>
                 <TODOInput />
                 <TODOList />
             </div>
