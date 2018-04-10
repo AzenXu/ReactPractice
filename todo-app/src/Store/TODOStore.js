3837e73f35499cdafd68b00e6dd59826682371c2
Azen Xu <xuchang08@meituan.com>
2018-04-10 13:06:43 +0800

    使用emitter告诉view 数据变了

--- todo-app/src/Store/TODOStore.js
+++ todo-app/src/Store/TODOStore.js
@@ -5,6 +5,13 @@
 import Constants from '../Constants/Constants';
 import TODODispatcher from '../Dispatcher/TODODispatcher';
 
+// ---
+// 为了传消息给View
+const CHANGE_TODOS = "CHANGE_TODOS";
+import EventEmitter from 'events';
+const _emitter = new EventEmitter;
+// ---
+
 // 1. 把数据放到Store中存储
 // 2. View取数据的时候来TODOStore中取
 let todos = [];
@@ -14,6 +21,16 @@ let TODOStore = {
     //  不能直接修改数据，只能通过Action来修改
     getTodos = () => {
         return todos;
+    },
+    //  Target: todos变了让View感知到，并刷新View
+    //  Action: 外界传进来一个闭包方法，todos变了就调这个闭包方法，把todos作为参数传出去...
+    //  方案二：使用EventEmitter这个东东，在合理的时间触发callBack
+    addObserver = (callBack) => {
+        //  又类似个通知中心... 监听CHANGE_TODOS这个消息的发出，执行callBack
+        _emitter.on(CHANGE_TODOS,callBack);
+
+        //  返回一个Dispossibal，用于view移除监听
+        return () => _emitter.removeListener(CHANGE_TODOS,callBack);
     },
     //  获取Dispatcher通知，相应action
     //  token的作用：项目中可能有多个store, 一个store想优先执行，则可以通过另一个store的token，让它先暂停一段时间
@@ -42,6 +59,9 @@ let TODOStore = {
             default:
                 break;
         }
+
+        //  action处理完之后，发通知
+        _emitter.emit(CHANGE_TODOS);
     }) // 在Dispatcher中注册下，所有action都会发进来
 }
 
