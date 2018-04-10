d15f1f6df0783f68bb98847c40ef69727fa6b38d
Azen Xu <xuchang08@meituan.com>
2018-04-10 12:47:11 +0800

    处理监听到消息调用之后对数据的处理

--- todo-app/src/Store/TODOStore.js
+++ todo-app/src/Store/TODOStore.js
@@ -19,29 +19,52 @@ let TODOStore = {
     _dispatchToken: TODODispatcher.register((action) => {
         switch (action.type) {
             case Constants.TOGGLEITEM:
-                
+                todos = _toggleItemList(todos, action.ID);
                 break;
 
             case Constants.DELETEITEM:
-
+                todos = _deleteItemList(todos, action.ID);
                 break;
 
             case Constants.CREATEITEM:
-
+                todos = _createItem(todos, action.title);
                 break;
 
             case Constants.EDITITEM:
-
+                "自己实现下";
                 break;
 
             case Constants.LOADDATA:
-
+                todos = action.todos;
                 break;
         
             default:
                 break;
         }
     }) // 在Dispatcher中注册下，所有action都会发进来
+}
+
+let _toggleItemList = (todos, ID) => {
+    let target = todos.find((item) => item.id === ID);
+    target.checked = !target.checked;
+    return todos;
+}
+
+let _deleteItemList = (todos, ID) => {
+    let idx = todos.findIndex((item) => item.id === ID); // 找到索引
+    todos.splice(idx, 1);
+    return todos;
+}
+
+let _createItem = (todos, title) => {
+    let lastID = todos.length > 0 ? todos[todos.length - 1].id : 100;
+    let newitem = {
+        id: lastID + 1,
+        title: title,
+        checked: false
+    }
+    todos.push(newitem);
+    return todos;
 }
 
 module.exports = TODOStore;
