a60ffd5967ec94027c8cea11c7094d5a55b009a0
Azen Xu <xuchang08@meituan.com>
2018-04-10 12:25:56 +0800

    Action类似于一个数据预处理中心，在里面处理包括「网络请求拉数据」之类的操作。预处理完成之后，通过Dispatcher把信息发通知。
    Const存了通知的常量名，Store接收到通知之后，拿到关键参数，操纵数据。

--- todo-app/src/Action/TODOAction.js
+++ todo-app/src/Action/TODOAction.js
@@ -1,18 +1,43 @@
 
+import Constants from '../Constants/Constants'
+import Dispatcher from '../Dispatcher/TODODispatcher'
+import TODODispatcher from '../Dispatcher/TODODispatcher';
+
 let TODOAction = {
     toggleItemList = (ID) => { // 不需要传todos的原因在于：store中存了需要改变的todos
-
+        TODODispatcher.dispatch({
+            id,
+            type: Constants.TOGGLEITEM
+        })
     },
     deleteItem = (ID) => {
-
+        TODODispatcher.dispatch({
+            id,
+            type: Constants.DELETEITEM
+        })
     },
     createItem = (title) => {
-
+        TODODispatcher.dispatch({
+            title,
+            type: Constants.CREATEITEM
+        })
     },
     editItem = (id, title) => {
-
+        TODODispatcher.dispatch({
+            id,
+            title,
+            type: Constants.EDITITEM
+        })
     },
     loadData = () => {
-
+        fetch('todos.json')
+            .then((response) => response.json())
+            .then((todos) => {
+                console.log(todos);
+                TODODispatcher.dispatch({
+                    todos,
+                    type: Constants.LOADDATA
+                })
+            })
     }
 }
