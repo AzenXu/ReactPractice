fbb8e647856f369734066d0e10e9cca79c0b10f4
Azen Xu <xuchang08@meituan.com>
2018-04-10 17:16:31 +0800

    【Flux重构】文件初始化

---
+++ counter-app/src/Dispatcher/CounterDispatcher.js
@@ -0,0 +1,5 @@
+import { Dispatcher } from 'flux'
+
+let CounterDispatcher = new Dispatcher();
+
+export default CounterDispatcher;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CounterApp from './CounterApp'
+import CounterApp from './View/CounterApp'
 
 ReactDOM.render(
     <CounterApp />, 
