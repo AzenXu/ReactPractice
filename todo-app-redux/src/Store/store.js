7b34490c58a59bfc9fd27cb58bd451b0ebf55aa0
Azen Xu <xuchang08@meituan.com>
2018-04-10 21:55:41 +0800

    【/Stroe实现 - 80%】通过手写trunkMiddleware实现兼容同步/异步Action & trunkMiddleware的原理

--- todo-app-redux/src/Store/store.js
+++ todo-app-redux/src/Store/store.js
@@ -3,7 +3,7 @@
  * 新Store相当于一个总管
  */
 
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import rootReducer from '../Reducers';
 
 //  需要通过combineReducers把小的Reducer汇总起来
@@ -12,6 +12,30 @@ import rootReducer from '../Reducers';
 //  Store实际上封装了所有的Reducer，Reducer仅和Store通信
 let combineReducers = combineReducers(rootReducer);
 
-let store = createStore(combineReducers);
+// ({ dispatch }) 这个是store中的dispatch
+//  thunkMiddleware 这个方法就是用来处理action传入的同步/异步东东的
+const thunkMiddleware = ({ dispatch }) => {
+
+    return (dispatch) => {
+
+        //  这里的action就是从TODOAction传进来的action
+        return (action) => {
+
+            //  判断action
+            if (typeof action === 'funciton') {
+
+                return aciton(dispatch); // 当action是函数的时候，把dispatch交给action，让action处理完之后手动回调
+            }
+            return dispatch(action); // 如果是个字典，直接通过dispatch分发
+        }
+
+    }
+}
+
+let store = createStore(
+    combineReducers,
+    applyMiddleware(thunkMiddleware)
+);
+
 
 export default store;
