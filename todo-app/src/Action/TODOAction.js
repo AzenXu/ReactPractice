cac04cd3e82037cfa1c585a660b58d185d680b31
Azen Xu <xuchang08@meituan.com>
2018-04-10 12:15:02 +0800

    准备好Action

---
+++ todo-app/src/Action/TODOAction.js
@@ -0,0 +1,18 @@
+
+let TODOAction = {
+    toggleItemList = (ID) => { // 不需要传todos的原因在于：store中存了需要改变的todos
+
+    },
+    deleteItem = (ID) => {
+
+    },
+    createItem = (title) => {
+
+    },
+    editItem = (id, title) => {
+
+    },
+    loadData = () => {
+
+    }
+}
