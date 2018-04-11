e54826c75996038cee596a7ed0e8644615e5c8b3
Azen Xu <xuchang08@meituan.com>
2018-04-11 19:18:50 +0800

    【布局】flex布局方式 - 按权重平分

--- RNDemo/App.js
+++ RNDemo/App.js
@@ -6,58 +6,25 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
-  View,
-  Button
+  View
 } from 'react-native';
-import style_outer from './styles';
-
-let Dimensions = require('Dimensions')
-
-let SCREEN_WIDTH = Dimensions.get('window').width;
-let SCREEN_HEIGHT = Dimensions.get('window').height;
-
-let style1 = {
-  backgroundColor:"#FF5566",
-  width: SCREEN_WIDTH,
-  height: SCREEN_HEIGHT
-}
 
 export default class App extends Component{
   render() {
     return (
-      <View style={style1}> {/* 对象样式 */}
-        <Text style={{
-          backgroundColor: '#66FF33',
-          color: '#110099',
-          fontSize: 40
-        }}> {/* 内联样式 */}
-          Welcome
-        </Text>
-        <Text style={[
-          {backgroundColor: '#559955'},
-          {width:300, height:100, borderLeftWidth:30}
-        ]}>
-          样式拼接
-        </Text>
-        <Text style={styles.s1}>
-          StyleSheet样式
-        </Text>
-        <Text style={style_outer.s1}>
-          样式分离
-        </Text>
+      <View style={styles.container}>
+        <View style={{backgroundColor:'green', flex:1}}></View>
+        <View style={{backgroundColor:'yellow', flex:2}}></View>
+        <View style={{ backgroundColor: 'red', flex: 3 }}></View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  s1: {
-    fontSize:30
+  container: {
+    flex:1,
+    backgroundColor: '#FF5566'
   }
-})
-// 样式是对象，那么就可以抽出来
-
-//  总结：Style是个对象，也可以是个装了很多对象的数组
+});
