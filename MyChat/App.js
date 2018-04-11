3cfba26c8985d8db4103d33f55a5c2dc8c556c1c
Azen Xu <xuchang08@meituan.com>
2018-04-11 20:20:55 +0800

    初始化

---
+++ MyChat/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Text>
+        呵呵呵
+      </Text>
+    );
+  }
+}
