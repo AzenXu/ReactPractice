d23911e6ab6aa128d5edca6378e3c3ac66cd6343
Azen Xu <xuchang08@meituan.com>
2018-04-11 19:29:50 +0800

    【布局】flexWrap用法超边自动换行显示

--- RNDemo/App.js
+++ RNDemo/App.js
@@ -7,16 +7,24 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View
+  View,
+  Text
 } from 'react-native';
 
 export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <View style={{backgroundColor:'green', flex:1}}></View>
-        <View style={{backgroundColor:'yellow', flex:2}}></View>
-        <View style={{ backgroundColor: 'red', flex: 3 }}></View>
+        <Text style={{width:100, height:100, backgroundColor: 'yellow', margin: 5}}>1</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5}}>2</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>3</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>4</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>5</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>6</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>7</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>8</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5}}>9</Text>
+        <Text style={{ width: 100, height: 100, backgroundColor: 'yellow', margin: 5 }}>10</Text>
       </View>
     );
   }
@@ -26,6 +34,7 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor: '#FF5566',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   }
 });
