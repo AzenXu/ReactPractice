1970bead29b4747ce0854032570dd89e8cef0045
Azen Xu <xuchang08@meituan.com>
2018-04-11 12:30:15 +0800

    【组件串联 99%】改造TODOApp以请求数据(不能通过TODOAction直接调函数，任何View想调Action，都需要connect之后，通过props调用)

--- todo-app-redux/src/View/TODOApp.js
+++ todo-app-redux/src/View/TODOApp.js
@@ -7,13 +7,14 @@ import React, { Component } from 'react';
 import TODOHeaderContainer from './TODOHeaderContainer';
 import TODOInputContainer from './TODOInputContainer';
 import TODOListContainer from './TODOListContainer';
+import TODOAction from '../Action/TODOAction';
 
-import TODOAction from '../Action/TODOAction'
+import { connect } from 'react-redux';
 
 class TODOApp extends Component {
 
     componentDidMount() {
-        TODOAction.loadData();
+        this.props.loadData();
     }
 
     render() {
@@ -27,4 +28,10 @@ class TODOApp extends Component {
     }
 }
 
-export default TODOApp;
+// export default TODOApp;
+export default connect(
+    undefined,
+    {
+        loadData: TODOAction.loadData
+    }
+)(TODOApp);
