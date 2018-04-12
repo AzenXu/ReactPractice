da70308cc7a8a0d3b584b3fcb746ce2b2c1ab4cd
Azen Xu <xuchang08@meituan.com>
2018-04-12 10:12:39 +0800

    【ListView】最简单使用

--- MyChat/MyListView.js
+++ MyChat/MyListView.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import {
     View,
-    StyleSheet,
-    Image,
-    Dimensions,
+    ListView,
     Text
 } from 'react-native';
 
 export default class MyListView extends Component {
-    
+
+    constructor (props, context) {
+        super(props, context)
+        let ds = new ListView.DataSource({
+            rowHasChanged:(r1,r2) => r1 != r2 // 代理方法，r1不等于r2的时候，做刷新
+        });
+        this.state = {
+            dataSource:ds.cloneWithRows(["1","2","3","4"])
+        }
+    }
+
     render() {
-        
+        return (
+            <ListView style={{flex:1, backgroundColor: 'green'}}
+                dataSource={this.state.dataSource}
+                renderRow={(rowItem)=>{ // rowItem就是dataSource里的item
+                    return <Text>{rowItem}</Text>
+                }}
+            />
+        )
     }
 }
