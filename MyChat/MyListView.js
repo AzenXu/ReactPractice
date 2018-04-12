0aadf56ce59707eb6d95b515be6f530dce0a6929
Azen Xu <xuchang08@meituan.com>
2018-04-12 10:43:12 +0800

    【ListView】变成collectionView

--- MyChat/MyListView.js
+++ MyChat/MyListView.js
@@ -13,7 +13,7 @@ export default class MyListView extends Component {
             rowHasChanged:(r1,r2) => r1 != r2 // 代理方法，r1不等于r2的时候，做刷新
         });
         this.state = {
-            dataSource:ds.cloneWithRows(["木木","灰灰","山山","猫猫"])
+            dataSource: ds.cloneWithRows(["木木", "灰灰", "山山", "猫猫", "灰灰", "山山", "猫猫", "灰灰", "山山", "猫猫"])
         }
     }
 
@@ -22,30 +22,22 @@ export default class MyListView extends Component {
             <ListView 
                 style={{flex:1, backgroundColor: 'green'}}
                 dataSource={this.state.dataSource}
-                // renderRow={(rowItem,sectionID,rowID) => { // rowItem就是dataSource里的item
-                //     return <Text style={
-                //         {marginTop:10, backgroundColor:'grey'}
-                //     }>{sectionID} + {rowID} + {rowItem}</Text>
-                // }}
                 renderRow={this._renderRow}
-                renderHeader={() => {
-                    return (
-                        <Text>renderHeader</Text>
-                    )
-                }
-                }
-                renderSectionHeader={() => {
-                    return (
-                        <Text>sectionHeader</Text>
-                    )
-                }}
+                contentContainerStyle={
+                    { 
+                        flexDirection: "row",
+                        flexWrap:"wrap", // 允许换行
+                        justifyContent:"space-around" // 沿主轴对齐方式
+                    }
+                } // 通过contentContainerStyle 设置排布方向
+                initialListSize={5} // 限制cell个数
             />
         )
     }
 
     _renderRow = (rowItem, sectionID, rowID) => {
         return <Text style={
-            { marginTop: 10, backgroundColor: 'grey' }
+            { margin: 10, backgroundColor: 'grey', height: 40, width: 60 }
         }>{sectionID} + {rowID} + {rowItem}</Text>
     }
 }
