04528de337e79ae8542d1a4c7e38a0a89849a92b
Azen Xu <xuchang08@meituan.com>
2018-04-12 10:32:29 +0800

    【ListView】SectionHeader等

--- MyChat/MyListView.js
+++ MyChat/MyListView.js
@@ -13,18 +13,39 @@ export default class MyListView extends Component {
             rowHasChanged:(r1,r2) => r1 != r2 // 代理方法，r1不等于r2的时候，做刷新
         });
         this.state = {
-            dataSource:ds.cloneWithRows(["1","2","3","4"])
+            dataSource:ds.cloneWithRows(["木木","灰灰","山山","猫猫"])
         }
     }
 
     render() {
         return (
-            <ListView style={{flex:1, backgroundColor: 'green'}}
+            <ListView 
+                style={{flex:1, backgroundColor: 'green'}}
                 dataSource={this.state.dataSource}
-                renderRow={(rowItem)=>{ // rowItem就是dataSource里的item
-                    return <Text>{rowItem}</Text>
+                // renderRow={(rowItem,sectionID,rowID) => { // rowItem就是dataSource里的item
+                //     return <Text style={
+                //         {marginTop:10, backgroundColor:'grey'}
+                //     }>{sectionID} + {rowID} + {rowItem}</Text>
+                // }}
+                renderRow={this._renderRow}
+                renderHeader={() => {
+                    return (
+                        <Text>renderHeader</Text>
+                    )
+                }
+                }
+                renderSectionHeader={() => {
+                    return (
+                        <Text>sectionHeader</Text>
+                    )
                 }}
             />
         )
+    }
+
+    _renderRow = (rowItem, sectionID, rowID) => {
+        return <Text style={
+            { marginTop: 10, backgroundColor: 'grey' }
+        }>{sectionID} + {rowID} + {rowItem}</Text>
     }
 }
