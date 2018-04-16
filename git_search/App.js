84d86120ef0ea28c9674a18942ccdb1493dd7593
Azen Xu <xuchang08@meituan.com>
2018-04-16 17:50:48 +0800

    【UI布局】初始化ListView

--- git_search/App.js
+++ git_search/App.js
@@ -11,13 +11,33 @@ import {
   Text,
   View,
   TextInput,
+  ListView
 } from 'react-native';
 
 export default class App extends Component {
+
+  constructor(props, context){
+    super(props, context);
+
+    let ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
+    this.state = {
+      dataSource: ds.cloneWithRows(["1","2","3"])
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <TextInput style={styles.textInput} />
+        <ListView 
+          enableEmptySections={true}
+          dataSource={this.state.dataSource}
+          renderRow={() => {
+            return <Text>Row</Text>
+          }}
+        />
       </View>
     );
   }
