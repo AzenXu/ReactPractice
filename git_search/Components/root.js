46c72fdc8bb79a0d9fb70287f338649dd785f5c0
Azen Xu <xuchang08@meituan.com>
2018-04-19 12:43:10 +0800

    【Redux改造】包装Component

---
+++ git_search/Components/root.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import React, { Component } from 'react';
+import { Provider } from "react-redux";
+import store from '../Store'
+
+
+ import App from './App'
+
+export default class Root extends Component {
+    render() {
+        return(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        );
+    }
+}
--- git_search/index.js
+++ git_search/index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Root from './Components/root';
 
-AppRegistry.registerComponent('git_search', () => App);
+AppRegistry.registerComponent('git_search', () => Root);
