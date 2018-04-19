152e7e1fc7bfe8198c475f220481cbb887b864eb
Azen Xu <xuchang08@meituan.com>
2018-04-19 12:35:15 +0800

    【Redux改造】初始化Store

---
+++ git_search/Store/index.js
@@ -0,0 +1,12 @@
+
+import { createStore, combineReducers,applyMiddleware } from "redux";
+import reducers from '../Reducers';
+import thunk from "redux-thunk";
+
+let composeReducer = combineReducers(reducers);
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
+const store = createStoreWithMiddleware(composeReducer);
+
+export default store;
